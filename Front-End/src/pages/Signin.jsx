fc8ae5418781f5e341ee83ec898d82cd985c4088
SinfulBan1 <sinfulban@gmail.com>
2025-10-04 15:43:12 -0400

    SignIn functionality

--- Front-End/src/pages/Signin.jsx
+++ Front-End/src/pages/Signin.jsx
@@ -1,8 +1,33 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 export default function SignIn () {
+    const {email, setEmail} = useState("");
+    const {password, setPassword} = useState("");
+    const {error, setError} = useState("");
+
+    const navigate = useNavigate();
+
+    async function logIn () {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/reserve');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
     return(
         <>
         <h3>Sign In</h3>
-        <input type="text" id="username" placeholder="Enter your username"></input>
+        {error && <p>{error}</p>}
+        <input
+        type="text"
+        id="username"
+        placeholder="Enter your username"
+        value = {email}
+        onChange={e => setEmail(e.target.value)}/>
         </>
     );
 }
