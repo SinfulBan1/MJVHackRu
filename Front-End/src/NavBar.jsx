4010ba313f9cc6fcad2ca802c031e7878e8460ae
SinfulBan1 <sinfulban@gmail.com>
2025-10-04 18:53:06 -0400

    User Sign In

--- Front-End/src/NavBar.jsx
+++ Front-End/src/NavBar.jsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
+import useUser from './useUser';
+import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
 
 export default function NavBar () {
+    const { isLoading, user } = useUser();
+
+    const navigate = useNavigate();
+
     return(
-        <>
         <nav className="navbar">
             <div className="navbar-left">
                 <Link to="/">
@@ -13,11 +19,22 @@ export default function NavBar () {
                 <Link to="/reserve" className="navbar-link">Reserve</Link>
             </div>
             <div className="navbar-right">
-                <Link to="/signin">
-                <button className="navbar-signin">Sign In</button>
-                </Link>
+                { isLoading ? <li>loading...</li> : (
+                    <>
+                    {user && (
+                        <>
+                            Logged in as {user.email}
+                        </>
+                    )}
+                    <>
+                        {user
+                            ? <button className="navbar-signin" onClick={() =>signOut(getAuth())}>Sign Out</button>
+                            : <button className="navbar-signin" onClick={() =>navigate('/signin')}>Sign In</button>}
+                    </>
+                    </>
+                )}
+
             </div>
         </nav>
-        </>
     );
 }
--- Front-End/src/pages/Signin.jsx
+++ Front-End/src/pages/Signin.jsx
@@ -3,13 +3,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function SignIn () {
-    const {email, setEmail} = useState("");
-    const {password, setPassword} = useState("");
-    const {error, setError} = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
     const navigate = useNavigate();
 
     async function logIn () {
+        console.log("LogIn function called");
         try {
             await signInWithEmailAndPassword(getAuth(), email, password);
             navigate('/reserve');
@@ -20,7 +21,7 @@ export default function SignIn () {
 
     return(
         <>
-        <h3>Sign In</h3>
+        <h1>Sign In</h1>
         {error && <p>{error}</p>}
         <input
             type="text"
@@ -29,10 +30,10 @@ export default function SignIn () {
             value = {email}
             onChange={e => setEmail(e.target.value)}/>
         <input
-            type="text"
+            type="password"
             id="password"
             placeholder="Enter your password"
-            value = {password}
+            value={password}
             onChange={e => setPassword(e.target.value)}/>
         <button onClick={logIn}>Log In</button>
         </>
