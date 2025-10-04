3afca6fc324cb91773f5bab07db1beaef1a24be5
SinfulBan1 <sinfulban@gmail.com>
2025-10-04 15:26:40 -0400

    Started signing

---
+++ Front-End/src/pages/Signin.jsx
@@ -0,0 +1,8 @@
+export default function SignIn () {
+    return(
+        <>
+        <h3>Sign In</h3>
+        <input type="text" id="username" placeholder="Enter your username"></input>
+        </>
+    );
+}
