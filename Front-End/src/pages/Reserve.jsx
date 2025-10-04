23ae6f266e8b9b74866e8c2a578d5b84857d2922
JZ-void <JZ.void21@gmail.com>
2025-10-04 14:45:34 -0400

    added seating and reserve page

---
+++ Front-End/src/pages/Reserve.jsx
@@ -0,0 +1,13 @@
+export default function Reserve () {
+    return(
+        <>
+        <h1>Reserve now!</h1>
+        <p>Click the number of your desired seat below. If you do not get your desired seat, find the next possible option.
+            Prices of seats vary depending on where the seat is located on the bus. Prices for seats are: 
+                Seats 25-28: $4
+                Seats 1-16: $5
+                Seats 17-24 and 29-33: $6
+        </p>
+        </>
+    );
+}
