22bdc36c8f020ab30ffb0f4954da6fee63153533
SinfulBan1 <sinfulban@gmail.com>
2025-10-05 15:23:15 -0400

    Added nodemon and basic postman testing endpoints

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get ('/hello', function (req, res) {
-    res.send("Hello!");
+    res.send("Hello! from get");
+});
+
+app.post ('/hello', function (req, res) {
+    res.send("Hello from a POST endpoint! Body: " + req.body.name);
 });
 
 app.listen(8000, function() {
